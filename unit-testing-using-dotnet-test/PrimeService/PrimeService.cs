452ce6e509b18ba5b525444a1b27ead6df63cbf6
Jason Steinshouer <jason.steinshouer@gmail.com>
2024-07-18 13:09:11 +0000

    additional tests

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -5,8 +5,8 @@ namespace Prime.Services
     public class PrimeService
     {
         public bool IsPrime(int candidate)
-{
-            if (candidate == 1)
+        {
+            if (candidate < 2)
             {
                 return false;
             }
